b407d3a514d3539c081cb8ad909f0227ea2e875c
cameronjohnson30 <cameron_johnson91@icloud.com>
2022-02-28 10:39:39 -0800

    add img, deleted console.logs, fixed indentations, styled css

--- js/scripts.js
+++ js/scripts.js
@@ -3,43 +3,35 @@ function customPizza(Size, toppings){
   this.toppings = toppings;
   this.price = 0;
 }
-console.log("fun")
 
 //price based on size and toppings 
-customPizza.prototype.piePrice= function() {
+customPizza.prototype.piePrice= function(){
   let toppingsPrice = this.toppings.length -2; 
   let pPrice = 0;
   let size = this.Size.toString();
-  console.log(size)
-if (size === "Small") {
-    pPrice += 8;  return this.price = this.price +pPrice + toppingsPrice;
-} else if (size === "Medium") {
-    pPrice+= 15;  return this.price = this.price +pPrice + toppingsPrice;
-  } else if (size === "Large") {
-    pPrice+= 20;   return this.price = this.price +pPrice + toppingsPrice;
-} else if (size === "Family") {
-    pPrice+= 30;  return this.price = this.price +pPrice + toppingsPrice;
-} 
-  return this.price = toppingsPrice + pPrice;
-}
+    if (size === "Small"){
+      pPrice += 8;
+  } else if (size === "Medium"){
+      pPrice+= 15;  
+  } else if (size === "Large"){
+      pPrice+= 20;   
+  } else if (size === "Family"){
+      pPrice+= 30;  
+  } 
+    return this.price = toppingsPrice + pPrice;
+  }
 
 
 // UI function 
-$(document).ready(() => {
-  $("#form-one").submit((event) => {
-    event.preventDefault();
+$(document).ready(() =>{
+  $("#form-one").submit((e) =>{
+    e.preventDefault();
     this.Size = $("#Size").val();
-    console.log("hi")
-    console.log(Size);
     let toppings = [];
     $("input[name='toppings']:checked").each(function(){
-     toppings.push($(this).val());
+      toppings.push($(this).val());
     });
-    console.log(toppings);
     newPizza = new customPizza(Size, toppings,);
-    $("#Price").text("$"+ newPizza.piePrice() + " dollars" + "  " + toppings + " YUMMMMM");
-    console.log(newPizza.piePrice())
-    console.log("hi");
+    $("#Price").text("$"+ newPizza.piePrice() + " dollars" + "  " + toppings + " YUMMMMM");  
   })
 });
-
